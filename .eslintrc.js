4f05caba8d82b4672e8cf3312228c7254b21af3c
迷渡 <justjavac@gmail.com>
2020-05-27 15:43:42 +0800

    移除 eslint-plugin-header
    Signed-off-by: 迷渡 <justjavac@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@
  */
 
 const OFF = 0;
-const WARNING = 1;
+// const WARNING = 1;
 const ERROR = 2;
 
 module.exports = {
@@ -23,7 +23,7 @@ module.exports = {
     allowImportExportEverywhere: true,
   },
   extends: ['airbnb', 'prettier', 'prettier/react'],
-  plugins: ['react-hooks', 'header'],
+  plugins: ['react-hooks'],
   rules: {
     // Ignore certain webpack alias because it can't be resolved
     'import/no-unresolved': [
@@ -31,26 +31,6 @@ module.exports = {
       {ignore: ['^@theme', '^@docusaurus', '^@generated']},
     ],
     'import/extensions': OFF,
-    'header/header': [
-      ERROR,
-      'block',
-
-      [
-        '*',
-        ' * Copyright (c) Facebook, Inc. and its affiliates.',
-        ' *',
-        ' * This source code is licensed under the MIT license found in the',
-        ' * LICENSE file in the root directory of this source tree.',
-        ' *',
-        // Unfortunately eslint-plugin-header doesn't support optional lines.
-        // If you want to enforce your website JS files to have @flow or @format,
-        // modify these lines accordingly.
-        {
-          pattern: '.* @format',
-        },
-        ' ',
-      ],
-    ],
     'react/jsx-closing-bracket-location': OFF, // Conflicts with Prettier.
     'react/jsx-filename-extension': OFF,
     'react-hooks/rules-of-hooks': ERROR,
